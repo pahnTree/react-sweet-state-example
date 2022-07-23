00d4d1cfa5e34a1d9c01a95f3c77302c630624a1
Philip Ahn <philipahn@outlook.com>
2022-07-22 22:24:05 -0400

    monorepo

---
+++ packages/app/src/pages/IndexPage.tsx
@@ -0,0 +1,15 @@
+import { Auth } from "../components/auth"
+
+const IndexPage = () => {
+  return (
+    <div>
+      <div>Index Page</div>
+      <div>
+        <h1>Auth test</h1>
+        <Auth />
+      </div>
+    </div>
+  )
+}
+
+export default IndexPage
